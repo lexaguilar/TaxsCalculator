5fc7ef0ffc00367ae229af9d7490d10dc2d07d47
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-04-11 16:38:46 -0600

    Inicializar repositorio

---
+++ data.js
@@ -0,0 +1,17 @@
+const salaryRanges = [
+    { from : 0.01, to: 100_000, base : 0, percent: 0, excess: 0  }
+    ,{ from : 100_000.01, to: 200_000, base : 0, percent: 0.15, excess: 100_000  }
+    ,{ from : 200_000.01, to: 350_000, base : 15_000, percent: 0.2, excess: 200_000  }
+    ,{ from : 350_000.01, to: 500_000, base : 45_000, percent: 0.25, excess: 350_000  }
+    ,{ from : 500_000.01, to: undefined, base : 82_500, percent: 0.3, excess: 500_000  }
+]
+
+let inssTax = 7;
+
+const divisor = 12;
+
+const getInssTax = () => inssTax;
+
+const setInssTax = newTax => (inssTax = newTax, inssTax);
+
+export { salaryRanges, divisor, getInssTax, setInssTax }
---
+++ index.js
@@ -0,0 +1,84 @@
+import { salaryRanges, getInssTax, divisor } from "./data.js";
+
+//Evento click del boton calcular
+document.querySelector("#btn-calculateTax")
+    .addEventListener('click', e => {
+
+        const salaryElemt = document.querySelector("#salarioMensual");
+
+        if(salaryElemt.value)
+        {
+            const result = calculteTaxs(salaryElemt.value);           
+
+            let draw = drawInformation('.table-anual tbody');
+            draw(result);
+
+            draw = drawInformation('.table-mensual tbody');
+            draw(getMonthly(result));
+
+        }
+    }
+);
+
+const getMonthly = ({ salary, inss, ir, neto }) => objectSalary(toMonth(salary), toMonth(inss), toMonth(ir), toMonth(neto));
+
+const toMonth = value => value/divisor;
+
+const objectSalary = (salary, inss, ir, neto) => ({ salary, inss, ir, neto });
+
+const calculteTaxs = salary => {
+
+    if(salary <= 0)
+        return { salary: 0, inss: 0, ir: 0, neto: 0 }
+
+    const baseSalaryAnualy = salary * divisor;
+    const inssTax = getInssTax();
+
+    const inss = (baseSalaryAnualy * inssTax / 100);
+
+    const baseSalaryBrutoAnualy = baseSalaryAnualy - inss;
+
+    const salaryRange = salaryRanges.find(x => x.from <= baseSalaryBrutoAnualy && x.to >= baseSalaryBrutoAnualy || x.to == undefined);
+
+    const { base, percent, excess } = salaryRange;
+    
+    const ir = ((baseSalaryBrutoAnualy - excess) * percent) + base;
+    const neto = baseSalaryBrutoAnualy - ir;
+
+    return {
+        salary : baseSalaryAnualy,
+        inss,
+        ir,
+        neto
+    }
+}
+
+const round = value => parseFloat(value).toFixed(2);
+
+const drawInformation  = selector => ({salary, inss, ir, neto}) => {
+
+    let table = document.querySelector(selector);
+    table.innerHTML = '';
+
+    let tr = `<tr>
+        <td>${round(salary)}</td>
+        <td>${round(inss)}</td>
+        <td>${round(ir)}</td>
+        <td>${round(neto)}</td>
+    </tr>`;
+
+    table.innerHTML = tr;
+}
+
+document.querySelectorAll('a').forEach(el => {
+    el.addEventListener('click', e => show(e.target.id));
+})
+
+const show = prop => {
+
+    document.querySelector('.config').classList.remove('show');
+    document.querySelector('.main').classList.remove('show');
+
+    document.querySelector(`.${prop}`).classList.add('show');
+        
+}
